4e2fdc15e329e13d284d0156682269389167d7b7
ztNFny <ztNFny@users.noreply.github.com>
2021-07-23 14:37:26 +0200

    make markers a little larger - will cause some overlap with list markers but should be ok

--- resizeVectors.sh
+++ resizeVectors.sh
@@ -6,8 +6,8 @@ sed -i 's/android:width="30dp"/android:width="36dp"/g' marker_nopin.xml
 sed -i 's/android:height="30dp"/android:height="36dp"/g' marker_nopin.xml
 sed -i 's/android:width="24dp"/android:width="12dp"/g' dot*.xml
 sed -i 's/android:height="24dp"/android:height="12dp"/g' dot*.xml
-sed -i 's/android:width="24dp"/android:width="12dp"/g' marker*.xml
-sed -i 's/android:height="24dp"/android:height="12dp"/g' marker*.xml
+sed -i 's/android:width="24dp"/android:width="13dp"/g' marker*.xml
+sed -i 's/android:height="24dp"/android:height="13dp"/g' marker*.xml
 sed -i 's/android:height="24dp"/android:height="22dp"/g' type*.xml
 sed -i 's/android:width="24dp"/android:width="22dp"/g' type*.xml
 #sed -i 's/android:viewportHeight="24">/android:viewportHeight="26"><group android:translateY="2">/g' type*.xml
