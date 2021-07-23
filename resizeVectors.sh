a6537590e36407e65099bffd72793c18b29facaa
ztNFny <ztNFny@users.noreply.github.com>
2021-07-23 14:27:39 +0200

    markers need to be smaller to accomendate list marker. Experiment with dot marker size

--- resizeVectors.sh
+++ resizeVectors.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
-cd /home/lars/gitwork/cgeo/main/res/drawable/
+cd ~/gitwork/cgeo/main/res/drawable/
 sed -i 's/android:width="30dp"/android:width="36dp"/g' marker.xml
 sed -i 's/android:height="30dp"/android:height="36dp"/g' marker.xml
 sed -i 's/android:width="30dp"/android:width="36dp"/g' marker_nopin.xml
 sed -i 's/android:height="30dp"/android:height="36dp"/g' marker_nopin.xml
-sed -i 's/android:width="24dp"/android:width="11dp"/g' dot*.xml
-sed -i 's/android:height="24dp"/android:height="11dp"/g' dot*.xml
-sed -i 's/android:width="24dp"/android:width="14dp"/g' marker*.xml
-sed -i 's/android:height="24dp"/android:height="14dp"/g' marker*.xml
+sed -i 's/android:width="24dp"/android:width="12dp"/g' dot*.xml
+sed -i 's/android:height="24dp"/android:height="12dp"/g' dot*.xml
+sed -i 's/android:width="24dp"/android:width="12dp"/g' marker*.xml
+sed -i 's/android:height="24dp"/android:height="12dp"/g' marker*.xml
 sed -i 's/android:height="24dp"/android:height="22dp"/g' type*.xml
 sed -i 's/android:width="24dp"/android:width="22dp"/g' type*.xml
 #sed -i 's/android:viewportHeight="24">/android:viewportHeight="26"><group android:translateY="2">/g' type*.xml
